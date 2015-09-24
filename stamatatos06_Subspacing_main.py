9bf61370f2fb1c5c6a4314370301698cf57cf0da
tisoso <timosommer@gmx.de>
2015-09-24 12:29:18 +0200

    now its running nearly stable

--- stamatatos06_Subspacing_main.py
+++ stamatatos06_Subspacing_main.py
@@ -5,8 +5,12 @@ from numpy.random import randint
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.datasets import fetch_20newsgroups
-from numpy import power
+from numpy import power, float64
 from numpy import argmax
+from enable.savage.svg.css.transform import transform
+from sympy.mpmath import root
+from StdSuites.AppleScript_Suite import result
+from numpy import ndarray
 '''
 this script implements the methods of E. Stamatatos' paper 'AUTORSHIP ATTRIBUTION ON FEATURE SET SUBSPACING ENSEMBLES'
 
@@ -50,28 +54,32 @@ def k_randome_classifier(dataset,  n_max_feature_number, m_subspace_width , text
         singel_count_vectorizer = CountVectorizer( max_features=n_max_feature_number , encoding=text_encoding)
 
         wordSubspace =[]#wordSubspace is a list with the words for the new clasifier
+        used_features = []
         i = 0 # iterator
         while i < m_subspace_width:
 
             a_random_int = randint(n_max_feature_number)
+            while a_random_int in used_features:
+                a_random_int = randint(n_max_feature_number)
             wordSubspace.append(wordlist[a_random_int])
+            used_features.append(a_random_int)
             i=i+1
 
         #train the vectorizer on the new subspace
-        singel_count_vectorizer.fit(wordSubspace)
+        singel_count_vectorizer= CountVectorizer( max_features=m_subspace_width , encoding=text_encoding, vocabulary=wordSubspace).fit(wordSubspace)
         #build the vectors from the dataset with the traind vectroizer
         vectored_data = singel_count_vectorizer.transform(dataset.data)
         #normize it
         X_train_tfidf =tfid_transformer.fit_transform(vectored_data)
         #train the Classifier with the normed vector set
-        clf = LinearDiscriminantAnalysis().fit( X_train_tfidf.toarray(), trainSet.target)
+        clf = LinearDiscriminantAnalysis(solver='svd').fit( X_train_tfidf.toarray(), trainSet.target)
         #add the classifier an the vectorizer to the list
         classifierList.append(clf)
         vectorizerList.append(singel_count_vectorizer)
 
     #return a struct with the lists
     return Bunch( classifier= classifierList, vectorizer= vectorizerList) ;
-def exhausiv_disjoint_subspacing(dataset,  n_max_feature_number, m_subspace_width , text_encoding):
+def exhaustiv_disjoint_subspacing(dataset,  n_max_feature_number, m_subspace_width , text_encoding):
     ''' Select m randomly chosen features from the data set and erase them from it. so you use ever feature only once
     '''
     ########## create feature list
@@ -85,14 +93,17 @@ def exhausiv_disjoint_subspacing(dataset,  n_max_feature_number, m_subspace_widt
     #list for passing the objects
     classifierList=[]
     vectorizerList =[]
+    
     used_features = []
     #nomilizer for the featuresets
+    singel_count_vectorizer = CountVectorizer( max_features=m_subspace_width , encoding=text_encoding)
+      
     tfid_transformer= TfidfTransformer( use_idf=False)
     k =0
     while k < int(n_max_feature_number/m_subspace_width):
         k = k+1
         #get new vectorizer trained on the features in the special subspace
-        singel_count_vectorizer = CountVectorizer( max_features=n_max_feature_number , encoding=text_encoding)
+        #singel_count_vectorizer = CountVectorizer( max_features=n_max_feature_number , encoding=text_encoding)
         wordSubspace =[] #wordSubspace is a list with the words for the new clasifier
         i = 0 # iterator
         while i < m_subspace_width:
@@ -108,64 +119,92 @@ def exhausiv_disjoint_subspacing(dataset,  n_max_feature_number, m_subspace_widt
 
 
         #train the vectorizer on the new subspace
-        singel_count_vectorizer.fit(wordSubspace)
+        singel_count_vectorizer= CountVectorizer( max_features=m_subspace_width , encoding=text_encoding, vocabulary=wordSubspace).fit(wordSubspace)
         #build the vectors from the dataset with the traind vectroizer
         vectored_data = singel_count_vectorizer.transform(dataset.data)
+    
+        
         #normize it
         X_train_tfidf =tfid_transformer.fit_transform(vectored_data)
+
         #train the Classifier with the normed vector set
-        clf = LinearDiscriminantAnalysis().fit( X_train_tfidf.toarray(), trainSet.target)
+        clf = LinearDiscriminantAnalysis(solver='svd').fit( X_train_tfidf.toarray(), trainSet.target)
+        
         #add the classifier an the vectorizer to the list
         classifierList.append(clf)
         vectorizerList.append(singel_count_vectorizer)
         #return a struct with the lists
     return Bunch( classifier= classifierList, vectorizer= vectorizerList)
 
-def getting_mean(n_max_feature_number, m_subspace_width, classifier_bunch, test_text):
-    '''
-    getting the mean of the posterior probailities see formula 2 in the paper
-    '''
+def getting_mean(n_max_feature_number, m_subspace_width, classifier_bunch, testSet_data):
+    
     k= int (n_max_feature_number/m_subspace_width)
+    #while t in range(len(testSet_data)):
+       
     i=0
-    mean =[(0 , 0)]
+    test_vector = classifier_bunch.vectorizer[i].transform(testSet_data)
+        
+    mean =  classifier_bunch.classifier[i].predict_proba(test_vector)
+    #print i , mean
+    i +=1
     while i < k:
-        test_vector = classifier_bunch.vectorizer[i].transform(test_text)
+        test_vector = classifier_bunch.vectorizer[i].transform(testSet_data)
         mean = mean + classifier_bunch.classifier[i].predict_proba(test_vector)
+        print i , mean
         i += 1
 
-    return( mean * 1/k);
-def getting_product(n_max_feature_number, m_subspace_width, classifier_bunch, test_text):
+    return (mean * (1./k))
+
+
+
+
+def getting_product(n_max_feature_number, m_subspace_width, classifier_bunch, testSet_data):
     '''
     getting the mean of the posterior probailities see formula 3 in the paper
     '''
+   
     k= int (n_max_feature_number/m_subspace_width)
+    #while t in range(len(testSet_data)):
+       
     i=0
-    test_vector = classifier_bunch.vectorizer[i].transform(test_text)
-    mean =  classifier_bunch.classifier[i].predict_proba(test_vector)
+    test_vector = classifier_bunch.vectorizer[i].transform(testSet_data)
+        
+    product =  classifier_bunch.classifier[i].predict_proba(test_vector)*10
+    #print i , product
     i +=1
     while i < k:
-        test_vector = classifier_bunch.vectorizer[i].transform(test_text)
-        mean = mean * classifier_bunch.classifier[i].predict_proba(test_vector)
+        test_vector = classifier_bunch.vectorizer[i].transform(testSet_data)
+        product = product * classifier_bunch.classifier[i].predict_proba(test_vector)*10
+        #print i , product
         i += 1
 
-    return power(mean, (1/k));
-def mp(n_max_feature_number, m_subspace_width, classifier_bunch, test_text):
+    #productList.append( power(product, (1/k)));
+    
+    
+    return (power(product, 1./k) * power(1./10 , 1./k))
+def mp(n_max_feature_number, m_subspace_width, classifier_bunch, testSet_data):
     '''Average of Mean and Prduct  '''
-    mean = getting_mean(n_max_feature_number, m_subspace_width, classifier_bunch, test_text)
-    product= getting_product(n_max_feature_number, m_subspace_width, classifier_bunch, test_text)
+    mean = getting_mean(n_max_feature_number, m_subspace_width, classifier_bunch, testSet_data)
+   
+    product= getting_product(n_max_feature_number, m_subspace_width, classifier_bunch, testSet_data)
+   
     return (mean + product)/2
-def predict_class( n_max_feature_number, m_subspace_width, trainingset, test_text, text_encoding,  mode = exhausiv_disjoint_subspacing ):
-    # use k_randome_classifier or exhausiv_disjoint_subspacing for mode  exhausiv_disjoint_subspacing
+def predict_class( n_max_feature_number, m_subspace_width, trainingset, test_text, text_encoding,  mode = exhaustiv_disjoint_subspacing ):
+    # use k_randome_classifier or exhaustiv_disjoint_subspacing for mode  exhaustiv_disjoint_subspacing
     #works only for one text at the time
     classifier_set = mode(trainingset,  n_max_feature_number , m_subspace_width, text_encoding)
     predict_result = mp(n_max_feature_number, m_subspace_width, classifier_set, test_text)
     return trainingset.target_names[argmax(predict_result)]
-
-
+def perdict_with_trainset( n_max_feature_number, m_subspace_width, classifier_set,trainingset, test_text, text_encoding ):
+    #use k_randome_classifier or exhaustiv_disjoint_subspacing for mode  exhaustiv_disjoint_subspacing
+    #works only for one text at the time
+    predict_result = mp(n_max_feature_number, m_subspace_width, classifier_set, test_text)
+    print predict_result
+    return trainingset.target_names[argmax(predict_result)]
 ####################Settings##################
-n = 1000
-m = 5
-encoding_setting = 'ISO 8859-2'
+n = 800
+m = 2
+encoding_setting = 'ISO 8859-7'#'utf-8'
 
 categories = None
 ##############################################
@@ -185,14 +224,48 @@ categories = None
 #     for mac use this command in terminal 
 #        find . -name '*.DS_Store' -type f -delete
 
-#trainSet = load_files('HitchHikersQuotes'  , categories= categories)
-categories = ['alt.atheism', 'comp.graphics']
-#for a big data set use  fetch_20newsgroups
-trainSet = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'),  categories=categories)
-
-test_text = ["OK. Leave this to me. I'm British. I know how to queue."]
-#k_randome_classifier(trainSet, n, m, encoding_setting)
+trainSet = load_files('Vima-Authors/vima GB/training corpus' )
+testSet = load_files('Vima-Authors/vima GB/test corpus', categories=None )
+classifier_set = exhaustiv_disjoint_subspacing(trainSet,  n , m, encoding_setting)
 
 
 
-print predict_class(n, m, trainSet, test_text, encoding_setting)
+print '#########'
+
+'''    
+tfid_transformer =TfidfTransformer( use_idf=False)
+X_train_tfidf =tfid_transformer.fit_transform(test_vector)
+product =  classifier_set.classifier[1].predict_proba(X_train_tfidf)
+print product
+'''
+product = mp(n, m, classifier_set, testSet.data)
+
+print ' normal',product
+
+ergebnis = argmax(product ,1)
+score =0
+f=0
+while f < len(ergebnis):
+    if (ergebnis[f] == testSet.target[f]):
+        score +=1
+    else: 
+        print testSet.target_names[ergebnis[f]] , testSet.target_names[testSet.target[f]]
+    print 'f', f+1, 'score', score
+    f +=1
+
+percent = (float(score)/(f))*100
+
+print ' percentage' , percent
+
+print 'erg' ,ergebnis
+print 'target ' , testSet.target
+print 'author 3 ', testSet.target_names[3]
+### percentage #### 
+
+'''
+    print " vectorizer" 
+    helper= classifier_set.vectorizer[t].transform(test_set)
+    print helper
+    
+'''
+
