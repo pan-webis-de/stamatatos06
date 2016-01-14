1ca9a577dcbde0d5f293be9d8e4444319475b406
Martin Potthast <martin@potthast.biz>
2016-01-14 18:03:10 +0100

    Update and rename run.sh to tira.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH="/home/sommer15/anaconda2/bin:$PATH"
-source ~/.bashrc
-
-python /home/sommer15/run_Stamatatos06/stamatatos06_Subspacing_main.py $1 $2 $3 $4 $5 $6
---
+++ tira.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH="/home/sommer15/anaconda2/bin:$PATH"
+source ~/.bashrc
+
+python /home/sommer15/run_Stamatatos06/stamatatos06.py $1 $2 $3 $4 $5 $6
