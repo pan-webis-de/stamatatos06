8ed66f36490db6103a88e39773fe1289aa090110
Martin Potthast <martin@potthast.biz>
2016-01-14 18:07:47 +0100

    Update tira.sh

--- tira.sh
+++ tira.sh
@@ -2,4 +2,4 @@
 export PATH="/home/sommer15/anaconda2/bin:$PATH"
 source ~/.bashrc
 
-python /home/sommer15/run_Stamatatos06/stamatatos06.py $1 $2 $3 $4 $5 $6
+python /home/sommer15/run_Stamatatos06/stamatatos06.py $1 $2 $3 $4 'lsqr' 'auto'
